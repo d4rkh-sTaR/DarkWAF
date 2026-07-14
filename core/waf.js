039888bc41c25c06bd486ea0c388c178f335c2ea
Madhav Purohit <madhavpurohit200607@gmail.com>
2026-07-14 21:10:26 +0530

    fix(server): bind server to 0.0.0.0 for external access

--- core/waf.js
+++ core/waf.js
@@ -3,17 +3,24 @@ import { matchString, inspectData } from './SignatureMatching/SignatureMatching.
 
 export default function DarkWAF(req, res, next) {
     
+    // returns the rule name if detected something and returns null if ir doesn't find anything
     let rule_body = inspectData(req.body, activeSignatures);
     let rule_query = inspectData(req.query, activeSignatures);
+    let rule_path = inspectData(req.path, activeSignatures);
 
     if (rule_body) {
-        console.warn(`[BLOCKED] Rule triggered in request body: ${rule_body}`);
-        return res.status(403).json({ error: "Forbidden 403" })
+        console.warn(`[BLOCKED] Rule triggered in request body: ${rule_body}. IP address : ${req.ip}`);
+        return res.status(403).send('<pre>Forbidden 403</pre>');
     }
 
     if (rule_query) {
-        console.warn(`[BLOCKED] Rule triggered in request query: ${rule_query}`);
-        return res.status(403).json({ error: "Forbidden 403" })
+        console.warn(`[BLOCKED] Rule triggered in request query: ${rule_query}. IP address : ${req.ip}`);
+        return res.status(403).send('<pre>Forbidden 403</pre>');
+    }
+
+    if (rule_path) {
+        console.warn(`[BLOCKED] Rule triggered in URL: ${rule_path}. IP address : ${req.ip}`);
+        return res.status(403).send('<pre>Forbidden 403</pre>');
     }
 
     next();
